6abfcf8dd764997fcd0d174453f96495f6c12e60
alvaxu <xuwh@csvw.com>
2018-12-03 14:32:27 +0800

    commit all

---
+++ 2.zipkin-http(timeout）/service-ribbon/src/main/java/jar/web/HelloControler.java
@@ -0,0 +1,24 @@
+package jar.web;
+
+import jar.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloControler {
+
+
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/ribbon")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+
+}
